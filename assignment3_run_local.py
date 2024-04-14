8dbd677f79ec0ddce8be511a6e4e9c2c2f45d8da
rjohnston101 <127871270+rjohnston101@users.noreply.github.com>
2024-04-14 20:22:10 +1000

    14/4 update

--- assignment3_run_local.py
+++ assignment3_run_local.py
@@ -1,4 +1,5 @@
 import gym
+import matplotlib.pyplot as plt
 import simple_driving
 # import pybullet_envs
 import pybullet as p
@@ -9,6 +10,165 @@ import pickle
 import torch
 import random
 
+# Hyper parameters that will be used in the DQN algorithm
+
+EPISODES = 2500                # number of episodes to run the training for
+LEARNING_RATE = 0.00025         # the learning rate for optimising the neural network weights
+MEM_SIZE = 50000                # maximum size of the replay memory - will start overwritting values once this is exceed
+REPLAY_START_SIZE = 10000      # The amount of samples to fill the replay memory with before we start learning
+BATCH_SIZE = 32                 # Number of random samples from the replay memory we use for training each iteration
+GAMMA = 0.99                    # Discount factor
+EPS_START = 0.1                 # Initial epsilon value for epsilon greedy action sampling
+EPS_END = 0.0001                # Final epsilon value
+EPS_DECAY = 4 * MEM_SIZE        # Amount of samples we decay epsilon over
+MEM_RETAIN = 0.1                # Percentage of initial samples in replay memory to keep - for catastrophic forgetting
+NETWORK_UPDATE_ITERS = 5000     # Number of samples 'C' for slowly updating the target network \hat{Q}'s weights with the policy network Q's weights
+
+FC1_DIMS = 128                   # Number of neurons in our MLP's first hidden layer
+FC2_DIMS = 128                   # Number of neurons in our MLP's second hidden layer
+
+# metrics for displaying training status
+best_reward = 0
+average_reward = 0
+episode_history = []
+episode_reward_history = []
+np.bool = np.bool_
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+# for creating the policy and target networks - same architecture
+class Network(torch.nn.Module):
+    def __init__(self, env):
+        super().__init__()
+        self.input_shape = env.observation_space.shape
+        self.action_space = env.action_space.n
+
+        # build an MLP with 2 hidden layers
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(*self.input_shape, FC1_DIMS),   # input layer
+            torch.nn.ReLU(),     # this is called an activation function
+            torch.nn.Linear(FC1_DIMS, FC2_DIMS),    # hidden layer
+            torch.nn.ReLU(),     # this is called an activation function
+            torch.nn.Linear(FC2_DIMS, self.action_space)    # output layer
+            )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=LEARNING_RATE)
+        self.loss = nn.MSELoss()  # loss function
+
+    def forward(self, x):
+        return self.layers(x)
+
+# handles the storing and retrival of sampled experiences
+class ReplayBuffer:
+    def __init__(self, env):
+        self.mem_count = 0
+        self.states = np.zeros((MEM_SIZE, *env.observation_space.shape),dtype=np.float32)
+        self.actions = np.zeros(MEM_SIZE, dtype=np.int64)
+        self.rewards = np.zeros(MEM_SIZE, dtype=np.float32)
+        self.states_ = np.zeros((MEM_SIZE, *env.observation_space.shape),dtype=np.float32)
+        self.dones = np.zeros(MEM_SIZE, dtype=np.bool)
+
+    def add(self, state, action, reward, state_, done):
+        # if memory count is higher than the max memory size then overwrite previous values
+        if self.mem_count < MEM_SIZE:
+            mem_index = self.mem_count
+        else:
+            mem_index = int(self.mem_count % ((1-MEM_RETAIN) * MEM_SIZE) + (MEM_RETAIN * MEM_SIZE))  # avoid catastrophic forgetting, retain first 10% of replay buffer
+
+        self.states[mem_index]  = state
+        self.actions[mem_index] = action
+        self.rewards[mem_index] = reward
+        self.states_[mem_index] = state_
+        self.dones[mem_index] =  1 - done
+
+        self.mem_count += 1
+
+    # returns random samples from the replay buffer, number is equal to BATCH_SIZE
+    def sample(self):
+        MEM_MAX = min(self.mem_count, MEM_SIZE)
+        batch_indices = np.random.choice(MEM_MAX, BATCH_SIZE, replace=True)
+
+        states  = self.states[batch_indices]
+        actions = self.actions[batch_indices]
+        rewards = self.rewards[batch_indices]
+        states_ = self.states_[batch_indices]
+        dones   = self.dones[batch_indices]
+
+        return states, actions, rewards, states_, dones
+
+class DQN_Solver:
+    def __init__(self, env):
+        self.memory = ReplayBuffer(env)
+        self.policy_network = Network(env)  # Q
+        self.target_network = Network(env)  # \hat{Q}
+        self.target_network.load_state_dict(self.policy_network.state_dict())  # initially set weights of Q to \hat{Q}
+        self.learn_count = 0    # keep track of the number of iterations we have learnt for
+
+    # epsilon greedy
+    def choose_action(self, observation):
+        # only start decaying epsilon once we actually start learning, i.e. once the replay memory has REPLAY_START_SIZE
+        if self.memory.mem_count > REPLAY_START_SIZE:
+            eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+                math.exp(-1. * self.learn_count / EPS_DECAY)
+        else:
+            eps_threshold = 1.0
+        # if we rolled a value lower than epsilon sample a random action
+        if random.random() < eps_threshold:
+            return np.random.choice(np.array(range(9)), p=[0.1,0.125,0.1,0.025,0.025,0.025,0.15,0.3,0.15])    # sample random action with set priors (if we flap too much we will die too much at the start and learning will take forever)
+
+        # otherwise policy network, Q, chooses action with highest estimated Q-value so far
+        print(observation)
+        state = torch.tensor(observation).float().detach()
+        state = state.unsqueeze(0)
+        state = state[0]
+        #print(state)
+        self.policy_network.eval()  # only need forward pass
+        with torch.no_grad():       # so we don't compute gradients - save memory and computation
+            q_values = self.policy_network(state)
+        return torch.argmax(q_values).item()
+
+    # main training loop
+    def learn(self):
+        states, actions, rewards, states_, dones = self.memory.sample()  # retrieve random batch of samples from replay memory
+        states = torch.tensor(states, dtype=torch.float32)
+        actions = torch.tensor(actions, dtype=torch.long)
+        rewards = torch.tensor(rewards, dtype=torch.float32)
+        states_ = torch.tensor(states_, dtype=torch.float32)
+        dones = torch.tensor(dones, dtype=torch.bool)
+        batch_indices = np.arange(BATCH_SIZE, dtype=np.int64)
+
+        self.policy_network.train(True)
+        q_values = self.policy_network(states)                # get current q-value estimates (all actions) from policy network, Q
+        q_values = q_values[batch_indices, actions]           # q values for sampled actions only
+
+        self.target_network.eval()                            # only need forward pass
+        with torch.no_grad():                                 # so we don't compute gradients - save memory and computation
+            q_values_next = self.target_network(states_)      # target q-values for states_ for all actions (target network, \hat{Q})
+
+        q_values_next_max = torch.max(q_values_next, dim=1)[0]  # max q values for next state
+
+        q_target = rewards + GAMMA * q_values_next_max * dones  # our target q-value
+
+        loss = self.policy_network.loss(q_target, q_values)     # compute loss between target (target network, \hat{Q}) and estimated q-values (policy network, Q)
+        #compute gradients and update policy network Q weights
+        self.policy_network.optimizer.zero_grad()
+        loss.backward()
+        self.policy_network.optimizer.step()
+        self.learn_count += 1
+
+        # set target network \hat{Q}'s weights to policy network Q's weights every C steps
+        if  self.learn_count % NETWORK_UPDATE_ITERS == NETWORK_UPDATE_ITERS - 1:
+            print("updating target network")
+            self.update_target_network()
+
+    def update_target_network(self):
+        self.target_network.load_state_dict(self.policy_network.state_dict())
+
+    def returning_epsilon(self):
+        return self.exploration_rate
+
 ######################### renders image from third person perspective for validating policy ##############################
 # env = gym.make("SimpleDriving-v0", apply_api_compatibility=True, renders=False, isDiscrete=True, render_mode='tp_camera')
 ##########################################################################################################################
@@ -18,18 +178,73 @@ import random
 ##########################################################################################################################
 
 ######################### if running locally you can just render the environment in pybullet's GUI #######################
-env = gym.make("SimpleDriving-v0", apply_api_compatibility=True, renders=True, isDiscrete=True)
+env = gym.make("SimpleDriving-v0", apply_api_compatibility=True, renders=False, isDiscrete=True)
 ##########################################################################################################################
 
-state, info = env.reset()
+#######################################
+env.action_space.seed(0)
+random.seed(0)
+np.random.seed(0)
+torch.manual_seed(0)
+episode_batch_score = 0
+episode_reward = 0
+agent = DQN_Solver(env)  # create DQN agent
+plt.clf()
+
+for i in range(EPISODES):
+    state = env.reset()  # this needs to be called once at the start before sending any actions
+    while True:
+        # sampling loop - sample random actions and add them to the replay buffer
+        #state = state[0]
+        action = agent.choose_action(state)
+        #print(env.step(action))
+        state_, reward, done, _, info = env.step(action)
+        #print(state)
+        #print(state_)
+        agent.memory.add(state[0], action, reward, state_, done)
+
+        # only start learning once replay memory reaches REPLAY_START_SIZE
+        if agent.memory.mem_count > REPLAY_START_SIZE:
+            print("hello")
+            agent.learn()
+
+        state = state_
+        episode_batch_score += reward
+        episode_reward += reward
+
+        if done:
+            break
+
+    print("hello2")
+
+    episode_history.append(i)
+    episode_reward_history.append(episode_reward)
+    episode_reward = 0.0
+
+    # save our model every batches of 100 episodes so we can load later. (note: you can interrupt the training any time and load the latest saved model when testing)
+    if i % 100 == 0 and agent.memory.mem_count > REPLAY_START_SIZE:
+        #torch.save(agent.policy_network.state_dict(), "/content/drive/My Drive/Colab Notebooks/policy_network.pkl")
+        print("average total reward per episode batch since episode ", i, ": ", episode_batch_score/ float(100))
+        episode_batch_score = 0
+    elif agent.memory.mem_count < REPLAY_START_SIZE:
+        print("waiting for buffer to fill...")
+        episode_batch_score = 0
+
+plt.plot(episode_history, episode_reward_history)
+plt.show()
+
+##################################
+
+# state, info = env.reset()
 # frames = []
 # frames.append(env.render())
 
-for i in range(200):
-    action = env.action_space.sample()
-    state, reward, done, _, info = env.step(action)
-    # frames.append(env.render())  # if running locally not necessary unless you want to grab onboard camera image
-    if done:
-        break
+# for i in range(100):
+#     action = env.action_space.sample()
+#     state, reward, done, _, info = env.step(action)
+#     print("State = ", state)
+#     #frames.append(env.render())  # if running locally not necessary unless you want to grab onboard camera image
+#     if done:
+#         break
 
 env.close()
